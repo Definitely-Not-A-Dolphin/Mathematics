da52c9715479e410d3109f1e04c25fc69e48c749
Definitely Not A Dolphin <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2024-05-16 11:26:25 +0000

    defining pi added to repo.

---
+++ Definging-pi.py
@@ -0,0 +1,14 @@
+import math
+n = int(input("To what value of n do you want pi to be defined? "))
+total1 = 3
+dec1 = input("\nDo you want to know the single value or the values of all integers below that number? ")
+if(dec1 == "single"):
+  for i in range(1,n):
+    total1 += (-1)**(i+1) * 4/((2*i)*(2*i + 1)*(2*i + 2))
+  print("\nWhen n = " + str(n) + ": " + str(total1))
+if(dec1 == "all"):
+  print("\n")
+  for i in range(1,n+1):
+    total1 += (-1)**(i+1) * 4/((2*i)*(2*i + 1)*(2*i + 2))
+    print("When n = " + str(i) + ": " + str(total1))
+print("\nEnd")
