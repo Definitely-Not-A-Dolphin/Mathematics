306be4faf27d0bb8f9d61abaf0eadfd0ec099dfc
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2024-12-03 21:23:49 +0100

    Format 1

--- CppThingeys/Factorial.cpp
+++ CppThingeys/Factorial.cpp
@@ -4,22 +4,21 @@ using namespace std;
 unsigned long long factorial(int n);
 
 int main() {
-    
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
 
-    cout << endl << factorial(n) << endl;
-    
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  cout << endl << factorial(n) << endl;
 }
 
-unsigned long long factorial(int n){
+unsigned long long factorial(int n) {
 
-    unsigned long long total = 1;
+  unsigned long long total = 1;
 
-    for(int i = 1; i <= n; i++){
-        total *= i;
-    }
+  for (int i = 1; i <= n; i++) {
+    total *= i;
+  }
 
-    return total;
+  return total;
 }
--- CppThingeys/Primer.cpp
+++ CppThingeys/Primer.cpp
@@ -10,56 +10,53 @@ bool listChecker(list<string> searchedList, string searchedWord);
 
 int main() {
 
-    int number;
-    cout << "Enter a number: ";
-    cin >> number;
+  int number;
+  cout << "Enter a number: ";
+  cin >> number;
 
-    list<string> yesList = {"Yes", "yes","Yeah","yeah","Yea","yea"
-    ,"Ye","ye"}; 
+  list<string> yesList = {"Yes", "yes", "Yeah", "yeah",
+                          "Yea", "yea", "Ye",   "ye"};
 
-    string y;
-    cout << endl << "Do you want to know all the numbers up to that point? ";
-    cin >> y;
+  string y;
+  cout << endl << "Do you want to know all the numbers up to that point? ";
+  cin >> y;
 
-    if(listChecker(yesList, y)){
-        for(int k=1; k <= number;++k){
-            cout << endl << k << " is " << primer(k);
-        }
+  if (listChecker(yesList, y)) {
+    for (int k = 1; k <= number; ++k) {
+      cout << endl << k << " is " << primer(k);
     }
-    else{
-        cout << endl << number << " is " << primer(number);
-    }
-
+  } else {
+    cout << endl << number << " is " << primer(number);
+  }
 }
 
-string primer(int n){
-    int d = 1;
+string primer(int n) {
+  int d = 1;
 
-    for (int i = 1;i <= ceil((n+1)/2);++i){
-        if(n%i == 0){
-            d+=1;
-        }
-    };
+  for (int i = 1; i <= ceil((n + 1) / 2); ++i) {
+    if (n % i == 0) {
+      d += 1;
+    }
+  };
 
-    if(n == 1){
-        return "Not Prime";
-    }
-    if(d == 2){
-        return "Prime";
-    }
-    else{
-        return "Composite";
-    }
+  if (n == 1) {
+    return "Not Prime";
+  }
+  if (d == 2) {
+    return "Prime";
+  } else {
+    return "Composite";
+  }
 }
 
 bool listChecker(list<string> searchedList, string searchedWord) {
-    bool result = false;
+  bool result = false;
 
-    for(string comparer : searchedList) {
-        if(comparer == searchedWord){
-            result = true;
-        }
-    };
+  for (string comparer : searchedList) {
+    if (comparer == searchedWord) {
+      result = true;
+    }
+  };
 
-    return result;
+  return result;
 }
