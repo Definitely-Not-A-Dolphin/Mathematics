a640b14627674ca8f25f2ff92c7a44738a53121f
Derek Verduijn <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2025-04-12 12:10:50 +0000

    Added Templates (a bit)

--- C++/mathFunctions.cpp
+++ C++/mathFunctions.cpp
@@ -1,33 +1,64 @@
 #include <cmath>
 
+template <typename realAbsTypename> realAbsTypename realAbs(realAbsTypename n) {
+  if (n < 0) {
+    return -n;
+  };
+
+  return n;
+};
+
+template <typename floorTypename> int floor(floorTypename n) {
+
+  int counter = 0;
+
+  n = realAbs(n);
+
+  while (n >= 0) {
+    n--;
+    counter++;
+  };
+
+  return counter;
+};
+
 unsigned long long factorial(int n) {
 
   unsigned long long total = 1;
 
-  if (n != 0) {
-    for (int i = 1; i <= n; i++) {
-      total *= i;
-    };
+  if (n == 0 or n == 1) {
+    return 1;
+  };
+
+  for (int i = 2; i <= n; i++) {
+    total *= i;
   };
 
   return total;
 }
 
 long double inverseFactorial(int n) {
+
   long double total = 1;
 
-  if (n != 0) {
-    for (long double i = 1; i <= n; i++) {
-      total /= i;
-    };
+  if (n == 0 or n == 1) {
+    return 1;
+  };
+
+  for (long double i = 2; i <= n; i++) {
+    total /= i;
   };
 
   return total;
 }
 
-template <typename T>
-int integerPow(T n, int k) {
+template <typename realPowerIntTypename>
+realPowerIntTypename realPowInt(realPowerIntTypename n, int k) {
   unsigned long long result = 1;
+
+  if (k == 0) {
+    return 0;
+  }
 
   for (int p = 1; p <= k; p++) {
     result *= n;
@@ -48,6 +79,6 @@ bool isPrime(int n) {
   if (d == 2) {
     return true;
   }
-  
+
   return false;
 }
---
+++ C++/testFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "mathFunctions.cpp"
+
+int main() {
+
+  std::cout << floor(-69.420) << std::endl;
+
+  return 0;
+}
